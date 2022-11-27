11d5d357b26acfcc2f17a3de8fe63b48835632ad
Ian Campbell <campbellni@hendrix.edu>
2022-11-27 03:38:46 -0600

    trees are stupid

--- My project/Assets/TreeCollsionDetector.cs
+++ My project/Assets/TreeCollsionDetector.cs
@@ -10,6 +10,7 @@ public class TreeCollsionDetector : MonoBehaviour
     TreeInstance[] apples;
     TreeInstance[] pears;
     TreeInstance[] plums;
+    private TerrainData terrainData;
     private void Start()
     {
         Terrain terrain = GetComponent<Terrain>();
@@ -23,25 +24,56 @@ public class TreeCollsionDetector : MonoBehaviour
         plums = trees.Where(
             t => t.prototypeIndex == 3
             ).ToArray();
+        Debug.Log(apples.Length);
+        Debug.Log(pears.Length);
+        Debug.Log(plums.Length);
+        terrainData = terrain.terrainData;
+        StartCoroutine(checkTrees());
+        //
+        //MAKE A COPY, DELETE TREE OR REPLACE, THEN SET TERRAINDATA>TREEINSTANDCES AS COPY
 
     }
     public IEnumerator checkTrees()
     {
-        WaitForSeconds wait = new WaitForSeconds(0.3f);
+        WaitForSeconds wait = new WaitForSeconds(0.5f);
         while (true)
         {
             yield return wait;
             foreach (TreeInstance tree in apples)
             {
-                Debug.Log(Vector3.Distance(tree.position, player.transform.position));
+                var treeInstancePos = tree.position;
+                var localPos = new Vector3(treeInstancePos.x * terrainData.size.x, treeInstancePos.y * terrainData.size.y, treeInstancePos.z * terrainData.size.z);
+                var worldPos = Terrain.activeTerrain.transform.TransformPoint(localPos);
+                //Debug.Log(Vector3.Distance(tree.position, player.transform.position));
+                if (Vector3.Distance(worldPos, player.transform.position) < 5)
+                {
+                    Debug.Log("APPLESSS");
+                }
+                
             }
             foreach (TreeInstance tree in pears)
             {
-                Debug.Log(Vector3.Distance(tree.position, player.transform.position));
+                var treeInstancePos = tree.position;
+                var localPos = new Vector3(treeInstancePos.x * terrainData.size.x, treeInstancePos.y * terrainData.size.y, treeInstancePos.z * terrainData.size.z);
+                var worldPos = Terrain.activeTerrain.transform.TransformPoint(localPos);
+                //Debug.Log(Vector3.Distance(tree.position, player.transform.position));
+                if (Vector3.Distance(worldPos, player.transform.position) < 5)
+                {
+                    Debug.Log("PEAARSSS");
+                }
+                
+                
             }
             foreach (TreeInstance tree in plums)
             {
-                Debug.Log(Vector3.Distance(tree.position, player.transform.position));
+                var treeInstancePos = tree.position;
+                var localPos = new Vector3(treeInstancePos.x * terrainData.size.x, treeInstancePos.y * terrainData.size.y, treeInstancePos.z * terrainData.size.z);
+                var worldPos = Terrain.activeTerrain.transform.TransformPoint(localPos);
+                if (Vector3.Distance(worldPos, player.transform.position) < 5)
+                {
+                    Debug.Log("PLUMSSSS");
+                    Destroy(tree);
+                }
             }
         }
     }
@@ -51,12 +83,5 @@ public class TreeCollsionDetector : MonoBehaviour
 
     }
     // Start is called before the first frame update
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("What");
-        if (other.CompareTag("Player"))
-        {
-            Debug.Log("HOLY FUCK THIS WSORKS");
-        }
-    }
+    
 }
